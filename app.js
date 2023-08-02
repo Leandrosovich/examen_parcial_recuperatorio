3b8a51329bcfd3a4091c611d93e1b3ad891eb45d
leandro Vera <leandro.vera.ok@gmail.com>
2023-08-02 15:23:24 -0300

    cambios realizados para la inicializacion del servidor en el app.js

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 // Imports
 const cors = require('cors');
 const express = require('express');
-
+const helmet = require('helmet');
+const morgan = require('morgan');
 const path = require('path');
+require('dotenv').config();
+require('ejs');
+
 
 const app = express();
+const port = process.env.PORT || 5000
 
 // Middlewares
 // TODO: Implementar middlewares
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
+
+
+
+
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,6 +29,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./routes/reserva.routes'));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
+app.use((req, res, next) => {
+    return res.status(404).render('404');
+
+});
+
 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(port, () => console.log(`Server on http://localhost:${port}`));
