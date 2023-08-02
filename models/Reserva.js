45ac65746ed18d3ef195b32e8c8bceb1feccd35b
leandro Vera <leandro.vera.ok@gmail.com>
2023-08-02 15:31:24 -0300

    Realizacion del modelado de datos

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,1 +1,70 @@
-// TODO: Crear modelo de datos de Reserva
+//Modelo de datos de un cine.
+//MODELADO DE DATOS REALIZADO.
+const { DataTypes } = require("sequelize");
+
+const Reserva = sequelize.define('Reserva', {
+    
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    codigo: {
+        type: DataTypes.STRING(100),
+        allowNule: false,
+        unique: true,
+        defaultValue: new Date().getTime()
+    },
+    nombre: {
+        type: DataTypes.STRING(100),
+        allowNule: false,
+    },
+    apellido: {
+        type: DataTypes.STRING(100),
+        allowNule: false,
+    },
+    pelicula: {
+        type: DataTypes.STRING(100),
+        allowNule: false,
+    },
+    fecha_ticket: {
+        type: DataTypes.DATE,
+        allowNule: false,
+    },
+    fecha_pelicula: {
+        type: DataTypes.DATE,
+        allowNule: false,
+    },
+    butaca: {
+        type: DataTypes.INTEGER,
+        allowNule: false,
+    },
+    sala: {
+        type: DataTypes.INTEGER,
+        allowNule: false,
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNule: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNule: false,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    deletedAt: {
+        type: DataTypes.DATE,
+        allowNule: true
+    }
+}, {
+    createdAt: true,
+    updatedAt: true,
+    deletedAt: true,
+    tableName: 'Reserva'
+});
+
+Reserva.sync({ force: false }).then(() => {
+    console.log('Tabla de reserva creada');
+});
+
+module.exports = Cincepolis;
